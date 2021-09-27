39c3bf645f15eb036dbc7049fb098a23bf3ee40b
Tiago Linhares <tiago.linhares@live.com>
2021-09-27 13:42:20 -0300

    linearLayout

---
+++ app/src/main/java/com/example/tiagolinhares/agenda/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tiagolinhares.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+            List<String> alunos = new ArrayList<>(
+                   Arrays.asList("Alex", "Fran", "Jose", "Nardoni"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
+    }
+}
